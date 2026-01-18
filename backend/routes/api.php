64017d7486d3bb23f358322b875dc665d77cc3a9
Manuel <barcelonamanuelmarc@gmail.com>
2026-01-18 23:50:05 +0800

    Added and removed views

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,11 @@
 <?php
-
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::get(url: '/users', action: function (Request $request) {
+    return User::all();
 });

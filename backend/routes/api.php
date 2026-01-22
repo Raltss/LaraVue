d7d46bd85a2e893caa7da1f92dfe2f3b7d8be2c4
Manuel <barcelonamanuelmarc@gmail.com>
2026-01-22 14:57:18 +0800

    fixed post request using axios

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,6 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get(url: '/users', action: function (Request $request) {
-    return User::all();
+Route::get('/users', function (Request $request) {
+    return User::all(); 
 });
